8c7bdfc5d3874a35ff8a9d23c291e725cbd5bf47
tranbrandon1233 <70561420+tranbrandon1233@users.noreply.github.com>
2024-02-03 11:59:00 -0800

    Implemented weather API

---
+++ WeatherAPI.py
@@ -0,0 +1,91 @@
+from dotenv import load_dotenv
+import os
+import requests
+from langchain.tools import tool
+from urllib.parse import quote
+
+load_dotenv()
+WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
+WEATHER_API_HOST = 'http://api.weatherapi.com/v1/'
+WEATHER_SEARCH_PATH = 'current.json'
+
+def get_info(city:str) -> str:
+        """Queries the Weather API for the current weather condition based on the input city from the user.
+
+        Args:
+            city (str): The city of the user.
+        
+        Returns: A string of the current weather condition in the city the user is in.
+        """
+        weather_url_params = {
+            'q': city,
+            'key': WEATHER_API_KEY,
+        }
+        weather_url_params = weather_url_params or {}
+        url = '{0}{1}'.format(WEATHER_API_HOST,quote(WEATHER_SEARCH_PATH.encode('utf8')))
+        headers = {
+            'Authorization': 'Bearer %s' % WEATHER_API_KEY,
+        }
+        #print(u'Querying {0} ...'.format(url))
+
+        response = requests.request('GET', url, headers=headers, params=weather_url_params)
+        return response.json()
+@tool
+def find_weather_condition(city:str) -> str:
+        """Queries the Weather API for the current weather condition based on the input city from the user.
+
+        Args:
+            city (str): The city of the user.
+        
+        Returns: A string of the current weather condition in the city the user is in.
+        """    
+        return get_info(city)['current']['condition']['text']
+@tool
+def find_temp(city:str) -> float:
+        """Queries the Weather API for the current temperature in Fahrenheit based on the input city from the user.
+
+        Args:
+            city (str): The city of the user.
+        
+        Returns: A float of the current temperature in Fahrenheit of the city the user is in.
+        """        
+        return get_info(city)['current']['temp_f']
+@tool
+def find_humidity(city:str) -> int:
+        """Queries the Weather API for the current humidity as a percentage based on the input city from the user.
+
+        Args:
+            city (str): The city of the user.
+        
+        Returns: A integer of the current humidity as a percentage in the city the user is in.
+        """        
+        return get_info(city)['current']['humidity']
+    
+@tool
+def find_precip(city:str) -> float:
+        """Queries the Weather API for the current precipitation in inches based on the input city from the user.
+
+        Args:
+            city (str): The city of the user.
+        
+        Returns: A float of the precipitation in inches of the city the user is in.
+        """        
+        return get_info(city)['current']['precip_in']
+
+@tool
+def find_wind_speed(city:str) -> float:
+        """Queries the Weather API for the wind speed in miles per hour based on the input city from the user.
+
+        Args:
+            city (str): The city of the user.
+        
+        Returns: A float of the current wind speed in miles per hour of the city the user is in.
+        """        
+        return get_info(city)['current']['wind_mph']
+    
+print(find_weather_condition("New York"))
+print(find_temp("New York"))
+print(find_humidity("New York"))
+print(find_precip("New York"))
+print(find_wind_speed("New York"))
+
--- YelpAPI.py
+++ YelpAPI.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import os
 import BaseAgent
 from dotenv import load_dotenv
