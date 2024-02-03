248e6f257451f7c8912a1686a73f05fcc6ed2ccd
Bill Zhang <billzhangsc@gmail.com>
2024-02-03 00:46:40 -0800

    Quick history update

--- Agent.py
+++ Agent.py
@@ -1,6 +1,7 @@
 from langchain import hub
 from langchain.agents import AgentExecutor, create_openai_tools_agent
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import AIMessage, HumanMessage
     
 class BaseAgent:
     def __init__(self, tools):
@@ -9,10 +10,21 @@ class BaseAgent:
         llm = ChatOpenAI(model="gpt-4-turbo-preview", temperature=0)
         agent = create_openai_tools_agent(llm, tools, prompt)
         self.executor = AgentExecutor(agent, tools)
+        self.history = []
         
     def update_status(self, status):
         """Not implemented yet, should do a quick status update to the user"""
         pass
     
     def invoke(self, message):
-        return self.executor.invoke(message)
+        output =  self.executor.invoke({
+            "input": message,
+            "history": self.history 
+        })
+        
+        self.history.append([
+            HumanMessage(message),
+            AIMessage(output)
+        ])
+        
+        return output
