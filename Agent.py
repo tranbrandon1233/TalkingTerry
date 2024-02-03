513e1088928d94ba706e66d26f85d10a9b2fa17a
Bill Zhang <billzhangsc@gmail.com>
2024-02-03 00:25:56 -0800

    Initial agent

--- Agent.py
+++ Agent.py
@@ -1,1 +1,18 @@
+from langchain import hub
+from langchain.agents import AgentExecutor, create_openai_tools_agent
+from langchain_openai import ChatOpenAI
     
+class BaseAgent:
+    def __init__(self, tools):
+        """tools should be list of functions"""
+        prompt = hub.pull("hwchase17/openai-tools-agent")
+        llm = ChatOpenAI(model="gpt-4-turbo-preview", temperature=0)
+        agent = create_openai_tools_agent(llm, tools, prompt)
+        self.executor = AgentExecutor(agent, tools)
+        
+    def update_status(self, status):
+        """Not implemented yet, should do a quick status update to the user"""
+        pass
+    
+    def invoke(self, message):
+        return self.executor.invoke(message)
