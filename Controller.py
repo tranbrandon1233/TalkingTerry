4076627fb591ed90d61aa069005ae2b2e68e2f06
Bill Zhang <billzhangsc@gmail.com>
2024-02-03 15:20:10 -0800

    Adding controller agent

--- Controller.py
+++ Controller.py
@@ -1,15 +1,17 @@
 from langchain import hub
 from langchain.agents import AgentExecutor, create_openai_tools_agent
 from langchain_openai import ChatOpenAI
-
-class Agent:
+from langchain_core.messages import AIMessage, HumanMessage
+    
+class Controller:
     def __init__(self, tools):
         """tools should be list of functions"""
         prompt = hub.pull("hwchase17/openai-tools-agent")
         llm = ChatOpenAI(model="gpt-4-turbo-preview", temperature=0)
         agent = create_openai_tools_agent(llm, tools, prompt)
         self.executor = AgentExecutor(agent, tools)
-
+        self.history = []
+        
     def update_status(self, status):
         """Not implemented yet, should do a quick status update to the user"""
         pass
@@ -17,6 +19,12 @@ class Agent:
     def invoke(self, message):
         output =  self.executor.invoke({
             "input": message,
+            "history": self.history 
         })
+        
+        self.history.append([
+            HumanMessage(message),
+            AIMessage(output)
+        ])
         
         return output
