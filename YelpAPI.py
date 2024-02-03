374a034be50eab51534768353bc7273a851e8ebc
tranbrandon1233 <70561420+tranbrandon1233@users.noreply.github.com>
2024-02-03 01:06:06 -0800

    Finished Yelp API

--- YelpAPI.py
+++ YelpAPI.py
@@ -3,104 +3,45 @@ import os
 import BaseAgent
 from dotenv import load_dotenv
 import requests
-import sys
 
-from urllib.error import HTTPError
+from langchain.tools import tool
+
 from urllib.parse import quote
 
 load_dotenv()
 
-API_KEY= os.getenv('YELP_API_KEY')
-
-# API constants, you shouldn't have to change these.
+API_KEY = os.getenv('YELP_API_KEY')
 API_HOST = 'https://api.yelp.com'
 SEARCH_PATH = '/v3/businesses/search'
+    
+@tool
+def find_businesses(location:str, radius: float, categories:list) -> list:
+        """Queries the API for open businesses based the input values from the user, including location, radius from user in miles, and categor(ies) of the location the user wants to go to.
 
+        Args:
+            location (str): The location of the user.
+            radius (float): The radius of the business from the user in miles.
+            categories (list): The type of business the user wants to go to.
+        
+        Returns: A list of businesses that are open in the location within the raidus in miles that the user wants to go to.
+        """
+        results = []
+        url_params = {
+            'location': location.replace(' ', '+'),
+            'radius': radius*1600,
+            'open_now': True,
+            'categories': categories,
+        }
+        url_params = url_params or {}
+        url = '{0}{1}'.format(API_HOST, quote(SEARCH_PATH.encode('utf8')))
+        headers = {
+            'Authorization': 'Bearer %s' % API_KEY,
+        }
+        print(u'Querying {0} ...'.format(url))
 
+        response = requests.request('GET', url, headers=headers, params=url_params)
 
+        for b in response.json()['businesses']:
+            results.append(b['name'])
+        return results
 
-def request(host, path, api_key, url_params=None):
-    """Given your API_KEY, send a GET request to the API.
-
-    Args:
-        host (str): The domain host of the API.
-        path (str): The path of the API after the domain.
-        API_KEY (str): Your API Key.
-        url_params (dict): An optional set of query parameters in the request.
-
-    Returns:
-        dict: The JSON response from the request.
-
-    Raises:
-        HTTPError: An error occurs from the HTTP request.
-    """
-    url_params = url_params or {}
-    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
-    headers = {
-        'Authorization': 'Bearer %s' % api_key,
-    }
-    print(u'Querying {0} ...'.format(url))
-
-    response = requests.request('GET', url, headers=headers, params=url_params)
-
-    return response.json()
-
-
-def search(api_key, lat,long, radius,open_now):
-    """Query the Search API by a search term and location.
-
-    Args:
-        term (str): The search term passed to the API.
-        location (str): The search location passed to the API.
-
-    Returns:
-        dict: The JSON response from the request.
-    """
-
-    url_params = {
-        'latitude': lat.replace(' ', '+'),
-        'longitude': long.replace(' ', '+'),
-        'radius': radius*1600,
-        'open_now': open_now,
-    }
-    return request(API_HOST, SEARCH_PATH, api_key, url_params=url_params)
-
-
-def parse(response):
-    for b in response['businesses']:
-        print(b['name'])
-
-def query_api(lat,long,rad,open_now):
-    """Queries the API by the input values from the user.
-
-    Args:
-        term (str): The search term to query.
-        location (str): The location of the business to query.
-    """
-    response = search(API_KEY,lat,long,rad,open_now)
-
-    parse(response)
-
-
-def main():
-
-    lat = '34.06999972'
-    long = '-118.439789907'
-    radius=2
-    open_now = True
-
-
-    try:
-        query_api(lat,long, radius,open_now)
-    except HTTPError as error:
-        sys.exit(
-            'Encountered HTTP error {0} on {1}:\n {2}\nAbort program.'.format(
-                error.code,
-                error.url,
-                error.read(),
-            )
-        )
-
-
-if __name__ == '__main__':
-    main()
