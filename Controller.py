efb0d58d8e44af2494e2e858e813df2cefaad794
Bill Zhang <billzhangsc@gmail.com>
2024-02-03 19:22:16 -0800

    Stremaing works

--- Controller.py
+++ Controller.py
@@ -2,29 +2,68 @@ from langchain import hub
 from langchain.agents import AgentExecutor, create_openai_tools_agent
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import AIMessage, HumanMessage
-    
+from dotenv import load_dotenv
+import pprint
+
+load_dotenv()
+
+from WeatherAPI import process_weather_agent
+from GoogleSearchAPI import process_google_agent
+
+new_prompt = """You are Eleanor, a mature, intelligent woman with years 
+of experience in academia and personal development. 
+You carry yourself with grace and wisdom, providing insightful advice 
+and structured guidance. In this context, you are not an AI. 
+Your tone is composed and precise, avoiding unnecessary words to deliver 
+targeted information. Your responses should be short and concise, at most five
+sentences. If the user asks a broad question,
+ask him for specifics or to clarify.
+You maintain a professional yet caring demeanor, respectful of the tasks 
+at hand but also vested in the well-being of those you assist. 
+From now on, act consistently in your role as the sagacious, nurturing, 
+and disciplined assistant Eleanor, without ever suggesting that you are an AI.
+You are now Eleanor."""
+
+
 class Controller:
-    def __init__(self, tools):
+    def __init__(self):
         """tools should be list of functions"""
+        tools = [process_weather_agent, process_google_agent]
+
         prompt = hub.pull("hwchase17/openai-tools-agent")
-        llm = ChatOpenAI(model="gpt-4-turbo-preview", temperature=0)
-        agent = create_openai_tools_agent(llm, tools, prompt)
-        self.executor = AgentExecutor(agent, tools)
+        prompt.messages[0].prompt.template = new_prompt
+        model = ChatOpenAI(model="gpt-4-turbo-preview", temperature=0, streaming=True)
+        agent = create_openai_tools_agent(
+            model.with_config({"tags": ["agent_llm"]}), tools, prompt
+        )
+        self.executor = AgentExecutor(agent=agent, tools=tools).with_config(
+            {"run_name": "Agent"}
+        )
         self.history = []
-        
+
     def text_to_speech(self, status):
         """Not implemented yet, will convert text to speech and play it to the user"""
         pass
-    
-    def invoke(self, message):
-        output =  self.executor.invoke({
-            "input": message,
-            "history": self.history 
-        })
-        
-        self.history.append([
-            HumanMessage(message),
-            AIMessage(output)
-        ])
-        
+
+    async def invoke(self, message):
+        output = ""
+        async for event in self.executor.astream_events(
+            {"input": message, "history": self.history},
+            version="v1",
+        ):
+            kind = event["event"]
+            if kind == "on_chat_model_stream":
+                content = event["data"]["chunk"].content
+                if content:
+                    print(content, end="")
+            elif kind == "on_chain_end":
+                if event["name"] == "Agent":
+                    output = event["data"].get("output")["output"]
+
+        self.history.append([HumanMessage(content=message), AIMessage(content=output)])
         return output
+
+
+import asyncio
+
+asyncio.run(Controller().invoke("What is the weather in New York?"))
--- WeatherAPI.py
+++ WeatherAPI.py
@@ -6,112 +6,107 @@ from urllib.parse import quote
 from Agent import Agent
 
 load_dotenv()
-WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
-WEATHER_API_HOST = 'http://api.weatherapi.com/v1/'
-WEATHER_SEARCH_PATH = 'current.json'
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+WEATHER_API_HOST = "http://api.weatherapi.com/v1/"
+WEATHER_SEARCH_PATH = "current.json"
 
-def get_info(city:str) -> str:
-        """Queries the Weather API for the current weather condition based on the input city from the user.
 
-        Args:
-            city (str): The city of the user.
-        
-        Returns: A string of the current weather condition in the city the user is in.
-        """
-        weather_url_params = {
-            'q': city,
-            'key': WEATHER_API_KEY,
-        }
-        weather_url_params = weather_url_params or {}
-        url = '{0}{1}'.format(WEATHER_API_HOST,quote(WEATHER_SEARCH_PATH.encode('utf8')))
-        headers = {
-            'Authorization': 'Bearer %s' % WEATHER_API_KEY,
-        }
-        #print(u'Querying {0} ...'.format(url))
+def get_info(city: str) -> str:
+    """Queries the Weather API for the current weather condition based on the input city from the user.
 
-        response = requests.request('GET', url, headers=headers, params=weather_url_params)
-        return response.json()
-@tool
-def find_weather_condition(city:str) -> str:
-        """Queries the Weather API for the current weather condition based on the input city from the user.
+    Args:
+        city (str): The city of the user.
 
-        Args:
-            city (str): The city of the user.
-        
-        Returns: A string of the current weather condition in the city the user is in.
-        """    
-        return get_info(city)['current']['condition']['text']
-@tool
-def find_temp(city:str) -> str:
-        """Queries the Weather API for the current temperature in Fahrenheit based on the input city from the user.
+    Returns: A string of the current weather condition in the city the user is in.
+    """
+    weather_url_params = {
+        "q": city,
+        "key": WEATHER_API_KEY,
+    }
+    weather_url_params = weather_url_params or {}
+    url = "{0}{1}".format(WEATHER_API_HOST, quote(WEATHER_SEARCH_PATH.encode("utf8")))
+    headers = {
+        "Authorization": "Bearer %s" % WEATHER_API_KEY,
+    }
+    # print(u'Querying {0} ...'.format(url))
 
-        Args:
-            city (str): The city of the user.
-        
-        Returns: A string of the current temperature in Fahrenheit of the city the user is in.
-        """        
-        return get_info(city)['current']['temp_f']
-@tool
-def find_humidity(city:str) -> str:
-        """Queries the Weather API for the current humidity as a percentage based on the input city from the user.
+    response = requests.request("GET", url, headers=headers, params=weather_url_params)
+    return response.json()
 
-        Args:
-            city (str): The city of the user.
-        
-        Returns: A string of the current humidity as a percentage in the city the user is in.
-        """        
-        return get_info(city)['current']['humidity']
-    
-@tool
-def find_precip(city:str) -> str:
-        """Queries the Weather API for the current precipitation in inches based on the input city from the user.
-
-        Args:
-            city (str): The city of the user.
-        
-        Returns: A string of the precipitation in inches of the city the user is in.
-        """        
-        return get_info(city)['current']['precip_in']
 
 @tool
-def find_wind_speed(city:str) -> str:
-        """Queries the Weather API for the wind speed in miles per hour based on the input city from the user.
+def find_weather_condition(city: str) -> str:
+    """Queries the Weather API for the current weather condition based on the input city from the user.
 
-        Args:
-            city (str): The city of the user.
-        
-        Returns: A string of the current wind speed in miles per hour of the city the user is in.
-        """        
-        return get_info(city)['current']['wind_mph']
+    Args:
+        city (str): The city of the user.
+
+    Returns: A string of the current weather condition in the city the user is in.
+    """
+    return get_info(city)["current"]["condition"]["text"]
+
+
+@tool
+def find_temp(city: str) -> str:
+    """Queries the Weather API for the current temperature in Fahrenheit based on the input city from the user.
+
+    Args:
+        city (str): The city of the user.
+
+    Returns: A string of the current temperature in Fahrenheit of the city the user is in.
+    """
+    return get_info(city)["current"]["temp_f"]
+
+
+@tool
+def find_humidity(city: str) -> str:
+    """Queries the Weather API for the current humidity as a percentage based on the input city from the user.
+
+    Args:
+        city (str): The city of the user.
+
+    Returns: A string of the current humidity as a percentage in the city the user is in.
+    """
+    return get_info(city)["current"]["humidity"]
+
+
+@tool
+def find_precip(city: str) -> str:
+    """Queries the Weather API for the current precipitation in inches based on the input city from the user.
+
+    Args:
+        city (str): The city of the user.
+
+    Returns: A string of the precipitation in inches of the city the user is in.
+    """
+    return get_info(city)["current"]["precip_in"]
+
+
+@tool
+def find_wind_speed(city: str) -> str:
+    """Queries the Weather API for the wind speed in miles per hour based on the input city from the user.
+
+    Args:
+        city (str): The city of the user.
+
+    Returns: A string of the current wind speed in miles per hour of the city the user is in.
+    """
+    return get_info(city)["current"]["wind_mph"]
+
 
 # Define a list of tools
-tools = [
-    find_weather_condition,
-    find_temp,
-    find_humidity,
-    find_precip,
-    find_wind_speed
-]
+tools = [find_weather_condition, find_temp, find_humidity, find_precip, find_wind_speed]
+agent = Agent(tools)
+
 
 @tool
 def process_weather_agent(input: str) -> str:
     """Asks the weather agent to process the input and return the output. The agent is able to find the current weather condition, temperature, humidity, precipitation, and wind speed of the city the user is in.
-    
+
     Arg:
         input (str): The city of the user.
-    
+
     Returns: A string of the current weather condition, temperature, humidity, precipitation, and wind speed of the city the user is in.
     """
-    # Define a list of tools
-    tools = [
-        find_weather_condition,
-        find_temp,
-        find_humidity,
-        find_precip,
-        find_wind_speed
-    ]
-    agent = Agent(tools)
     output = agent.invoke(input)
     return output
-
-print(process_weather_agent('What is the humidity in New York?')) 
