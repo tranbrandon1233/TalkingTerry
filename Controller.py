a7b8f30d8128b802d5a1be3f5877759a83721cf7
tranbrandon1233 <70561420+tranbrandon1233@users.noreply.github.com>
2024-02-03 22:52:44 -0800

    Good prompt for Twilio

--- Controller.py
+++ Controller.py
@@ -1,6 +1,7 @@
 from langchain import hub
 from langchain.agents import AgentExecutor, create_openai_tools_agent
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import AIMessage, HumanMessage
 from dotenv import load_dotenv
 import pprint
 
@@ -8,6 +9,9 @@ load_dotenv()
 
 from WeatherAPI import process_weather_agent
 from GoogleSearchAPI import process_google_agent
+from TwilioAPI import process_phone_agent
+from YelpAPI import process_yelp_agent
+from BruinLearnAPI import process_bruinlearn_agent
 
 new_prompt = """You are Eleanor, a mature, intelligent woman with years 
 of experience in academia and personal development. 
@@ -26,8 +30,8 @@ You are now Eleanor."""
 
 class Controller:
     def __init__(self):
-        """tools should be list of functions"""
-        tools = [process_weather_agent, process_google_agent]
+        """tools should be list of functions""" 
+        tools = [process_weather_agent, process_google_agent, process_bruinlearn_agent, process_phone_agent, process_yelp_agent]
 
         prompt = hub.pull("hwchase17/openai-tools-agent")
         prompt.messages[0].prompt.template = new_prompt
@@ -46,24 +50,24 @@ class Controller:
 
     async def invoke(self, message):
         output = ""
-        full_message = "History: \n\n"
-        for index, i in enumerate(self.history):
-            if index % 2 == 0:
-                full_message += f"User: {i}\n"
-            else:
-                full_message += f"Agent: {i}\n"
-        full_message += f"User: {message}\n"
         async for event in self.executor.astream_events(
-            {"input": full_message, "history": self.history},
+            {"input": message, "history": self.history},
             version="v1",
         ):
             kind = event["event"]
             if kind == "on_chat_model_stream":
                 content = event["data"]["chunk"].content
                 if content:
-                    yield content
+                    print(content, end="")
             elif kind == "on_chain_end":
                 if event["name"] == "Agent":
                     output = event["data"].get("output")["output"]
-        print(f"Controller: invoke: message: {message}, output: {output}")
-        self.history.append([message, output])
+
+        self.history.append([HumanMessage(content=message), AIMessage(content=output)])
+        return output
+
+
+import asyncio
+
+#asyncio.run(Controller().invoke("What are my current classes? Send it as a text message to my phone number at 6262786314 using the Twilio API. Do not add a +1 to the phone number."))
+asyncio.run(Controller().invoke("What are my current classes? Send it as a text message to my phone number at 6262786314."))
