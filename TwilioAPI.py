2dd34d581367f50edc16843a9d05c7aef5e2a369
tranbrandon1233 <70561420+tranbrandon1233@users.noreply.github.com>
2024-03-02 13:38:07 -0800

    Update TwilioAPI.py

--- TwilioAPI.py
+++ TwilioAPI.py
@@ -34,8 +34,8 @@ def call_user(message:str,phone_number:str) -> str:
     client = Client(os.getenv('TWILIO_SID'), os.getenv('TWILIO_AUTH_TOKEN'))
     client.calls.create(
                         twiml='<Response><Say>'+message+'</Say></Response>',
-                        to='+16262786314',
-                        from_='+18883267630'
+                        to='+',
+                        from_='+'
     )
     return "Operation completed successfully."
 
